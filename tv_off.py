01fdefbff80b0e73d7799661e8c8da93873ede5d
Devon Henry <dchenry006@gmail.com>
2024-12-18 16:08:28 -0500

    add input function to tv_off.py

---
+++ tv_off.py
@@ -0,0 +1,13 @@
+def tv_off(n):
+
+    def gnx(n):
+        if n < 1:
+            n = 1
+        for i in range(round(n)):
+            yield 'A'
+    
+    print(f"MUST{''.join(gnx(n))}RD{'!' * round(n/10)}")
+
+n = input("Somebody gotta do it... but how many times?? ")
+
+tv_off(n)
