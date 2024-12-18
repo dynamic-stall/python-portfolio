58ebe4fdd82e25c07cabd1ffd21cd8dcd0eaddff
Devon Henry <dchenry006@gmail.com>
2024-12-18 18:39:11 -0500

    add UI theme and comments

--- tv_off.py
+++ tv_off.py
@@ -1,20 +1,31 @@
+# primarily a test to practice git-merge conflict resolution
+
 def tv_off(n):
+
+    import time
 
     def gnx(n):
         if n < 1:
             n = 1
         for i in range(round(n)):
             yield 'A'
-    
+
+    ui = 0.64
+    sfx = "DUN!"
+
+    for i in range(13):
+        print(sfx); time.sleep(ui)
+    print("DUN-\n")
+
+    while True:
+        try:
+            n = float(input("Somebody gotta do it... but how many times?? "))
+        except ValueError:
+            print("A numeric entry is required...")
+            continue
+        else:
+            break
+
     print(f"MUST{''.join(gnx(n))}RD{'!' * round(n/10)}")
 
-while True:
-    try:
-        n = float(input("Somebody gotta do it... but how many times?? "))
-    except ValueError:
-        print("A numeric entry is required...")
-        continue
-    else:
-        break
-
-tv_off(int(n))
+tv_off()
